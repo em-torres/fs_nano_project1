6465e9879833c70fdf9676b30356ba1e72ad2c73
Edwin Marte Torres <kdgpro15@gmail.com>
2017-06-05 22:40:37 -0400

    Committing last part of training and beginning project

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,10 +1,23 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie(
-    "https://upload.wikimedia.org/wikipedia/commons/d/db/Patern_test.jpg",
-    "Some Storyline",
-    "Una Pelicula mas",
-    "https://www.youtube.com/watch?v=hC8CH0Z3L54")
 
-fresh_tomatoes.open_movies_page([toy_story])
+tmdb = media.TmdbApiConnection()
+# for movie in tmdb.response:
+    # print(movie)
+
+# toy_story = media.Movie(
+#     2.5, "https://upload.wikimedia.org/wikipedia/commons/d/db/Patern_test.jpg",
+#     "Some Storyline",
+#     "Una Pelicula mas",
+#     "https://www.youtube.com/watch?v=hC8CH0Z3L54"
+# )
+#
+# toy_story2 = media.TvShow(
+#     2.5, "https://upload.wikimedia.org/wikipedia/commons/d/db/Patern_test.jpg",
+#     "Some Storyline",
+#     "Una Pelicula mas",
+#     "https://www.youtube.com/watch?v=hC8CH0Z3L54"
+# )
+#
+# fresh_tomatoes.open_movies_page([toy_story], [toy_story2])
--- media.py
+++ media.py
@@ -1,15 +1,41 @@
 import webbrowser
 
 
-class Movie:
+class Video:
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+
+class Movie(Video):
     """ This class provides a way to store movie related information """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, poster_image, storyline, title, trailer_youtube):
+    def __init__(self, duration, poster_image, storyline, title, trailer_youtube):
+        Video.__init__(self, title, duration)
         self.poster_image_url = poster_image
         self.storyline = storyline
-        self.title = title
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
+
+
+class TvShow(Movie):
+    def __init__(self, duration, episode, title, season, tv_station):
+        Video.__init__(self, title, duration)
+        self.episode = episode
+        self.season = season
+        self.tv_station = tv_station
+        pass
+
+
+class TmdbApiConnection:
+    def __init__(self):
+        self.browse = webbrowser.open('https://api.themoviedb.org/3/movie/latest?api_key=732919f76697dcf56e13cf7ee937118a&language=en-US')
+        # self.latest = tmdb.Movies.latest()
+
+
+
+
+    pass
